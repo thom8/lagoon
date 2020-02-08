84f23a61441d309863df633b6f03a4aebd90f945
Thom Toogood <thomtoogood@gmail.com>
2020-02-08 15:21:05 +1300

    WIP

--- helpers/shared-to-shared-migrate.sh
+++ helpers/shared-to-shared-migrate.sh
@@ -63,19 +63,19 @@ TIMESTAMP=$(date +%s)
 
 # Colours.
 shw_grey () {
-  echo $(tput bold)$(tput setaf 0) $@ $(tput sgr 0)
+  echo $(tput bold)$(tput setaf 0) "$@" $(tput sgr 0)
 }
 shw_norm () {
-  echo $(tput bold)$(tput setaf 9) $@ $(tput sgr 0)
+  echo $(tput bold)$(tput setaf 9) "$@" $(tput sgr 0)
 }
 shw_info () {
-  echo $(tput bold)$(tput setaf 4) $@ $(tput sgr 0)
+  echo $(tput bold)$(tput setaf 4) "$@" $(tput sgr 0)
 }
 shw_warn () {
-  echo $(tput bold)$(tput setaf 2) $@ $(tput sgr 0)
+  echo $(tput bold)$(tput setaf 2) "$@" $(tput sgr 0)
 }
 shw_err ()  {
-  echo $(tput bold)$(tput setaf 1) $@ $(tput sgr 0)
+  echo $(tput bold)$(tput setaf 1) "$@" $(tput sgr 0)
 }
 
 # Parse input arguments.
